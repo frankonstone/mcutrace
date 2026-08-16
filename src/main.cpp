d211902982a23914e4399277d21d15227f099f8d
frankonstone <frankonstone@netscape.net>
2026-08-16 21:54:06 +0200

    feat: run P7 CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
-#include <mcutrace/model.hpp>
+#include <mcutrace/cli.hpp>
 
 #include <iostream>
 
-int main() {
-    std::cout << "mcutrace 0.1.0\n";
-    return 0;
+int main(int argc, char* argv[]) {
+    const auto options = mcutrace::parse_cli(argc, argv);
+    if (!options) {
+        std::cerr << options.error().message << '\n';
+        return 2;
+    }
+    return mcutrace::run_cli(*options);
 }
